eed2f7993f9d90ecbc83efe64b90b1cc24a0688f
Nico <1916148+nrmarston@users.noreply.github.com>
2020-11-26 09:08:05 -0600

    Added noun and verb properties

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Wah gwaan World")
+let obj = {
+    noun: ['politics',
+        'passenger',
+        'conversation',
+        "category",
+        "agency",
+        "football",
+        "statement",
+        "heart",
+        "possibility",
+        "celebration"],
+        verb: ["bound",
+            "sign",
+            "have",
+            "embrace",
+            "struggle",
+            "convince",
+            "borrow",
+            "kick",
+            "weigh",
+            "test"],
+        adjective: []
+};
+
+//console.log(obj.noun[4]);
