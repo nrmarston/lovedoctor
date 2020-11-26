a6cc9bbeff3776c1347c2dfeb845afd018f22b16
Nico <1916148+nrmarston@users.noreply.github.com>
2020-11-26 09:28:13 -0600

    Random number function

--- app.js
+++ app.js
@@ -31,4 +31,11 @@ let obj = {
         "fanatical"]
 };
 
-console.log(obj.verb[3]);
+const randomNumber = (arr) => {
+    let number = Math.floor(Math.random(0)* arr.length);
+    return number;
+}
+
+console.log(randomNumber(obj.noun));
+console.log(randomNumber(obj.verb));
+console.log(randomNumber(obj.adjective));
